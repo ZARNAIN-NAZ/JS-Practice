98a61854406589e1a328da7abf67326c18411f28
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-07-13 03:31:46 +0500

    question1-3 done

---
+++ chap35-38.js
@@ -0,0 +1,29 @@
+//Q1
+// function displayDate(){
+//     var currentDate =  new Date();
+//     var datestring = currentDate.toLocaleString();
+
+//     document.write("current Date &7 Time" +datestring)
+// }
+// displayDate()
+
+//Q2
+// function greet(fname, lname){
+//     var fullname  =fname+lname;
+//     var greeting = "hello"+fullname
+//     return greeting;
+// }
+// var fname="zarnain"
+// var lname = "naz"
+// var msg = greet(fname , lname)
+// console.log(msg)
+
+function displaySum(num1 , num2){
+        var num1 = prompt("enter number")
+        var num2 = prompt("enter number")
+
+    var sum = num1+num2
+    // return sum;
+}
+console.log(sum)
+displaySum(2)
