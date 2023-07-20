cf6412a504e1f4d06713fdc40ec124fc05426a4f
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-07-20 21:19:20 +0500

    start 7 questions of 35-38

--- chap35-38.js
+++ chap35-38.js
@@ -18,12 +18,74 @@
 // var msg = greet(fname , lname)
 // console.log(msg)
 
-function displaySum(num1 , num2){
-        var num1 = prompt("enter number")
-        var num2 = prompt("enter number")
+// Q3
+// function displaySum(num1 , num2){
+//         var num1 = prompt("enter number")
+//         var num2 = prompt("enter number")
 
-    var sum = num1+num2
-    // return sum;
-}
-console.log(sum)
-displaySum(2)
+//     var sum = num1+num2
+//     // return sum;
+// }
+// console.log(sum)
+// displaySum(2);
+
+//Q4
+// function calculator(num1, num2, operate) {
+//     let calculation;
+//     switch(operate){
+//         case "+":
+//           calculation = num1 +num2;
+//           break;
+//           case "-":
+//           calculation = num1 -num2;
+//           break;
+//           case "*":
+//           calculation = num1 *num2;
+//           break;
+//           case "/":
+//           calculation = num1 /num2;
+//           break; 
+//  default:
+//     "invalid"
+//     return;
+//     }
+//     console.log(calculation)
+//     return calculation;
+// }
+// calculator(5,7,"-")
+
+//Q5
+// function myFunc(num1 ){
+//     let r = num1**2
+//  console.log(r)
+// }
+// myFunc(9)
+
+//Q6
+// function myFunc(val){
+
+//     if(val ===0 || val ===1){
+//         return 1;
+//     }
+//     else{
+//         return val * myFunc(val-1)
+//     }
+// }
+// console.log(myFunc(9))
+
+//Q7
+// function counting(start ,end){
+//   if(start <=end){
+//     for(let i = start; i<=end; i++){
+//         console.log(i)
+//     }
+//   }
+//   else{
+//     for(let  i = start ; i>=end ; i--){
+//         console.log(i)
+//     }
+//   }
+// }
+// console.log(counting(7,3))
+
+//Q8
