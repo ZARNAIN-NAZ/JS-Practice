06cbb44a4b10519e8ede56ed0a2c47bff7caa3bf
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-07-22 02:25:20 +0500

    updtaed questions

--- chap38-42.js
+++ chap38-42.js
@@ -23,4 +23,40 @@
 // console.log(area)
 
 // ===============================================Question 04===============================
+// function calculateAverage(s1,s2,s3){
+//     return (s1+s2+s3)/3;
+// }
+// function perc(s1,s2,s3){
+//     const total=s1+s2+s3;
+//     const subjects = 3;
+//     return(total/(subjects*100))*100;
+// }
+// function main(s1,s2,s3){
+// const avg = calculateAverage(s1,s2,s3)
+// const percentage = perc(s1,s2,s3)
+// console.log(s1)
+// console.log(s2)
+// console.log(s3)
+// console.log(avg)
+// console.log(percentage)
+
+// }
+// console.log(main(94,86,83))
+// ===============================================Question 05===============================
+// function  index(str, char){
+//     for(let i=0; i<str.length;i++){
+//         if(str[i]===char){
+//             return i
+//         }
+//     }
+//     return -1;
+// }
+// console.log(index("sbhhjh","s"))
+
+// ===============================================Question 06===============================
+function dlt(vowels){
+    const sen= vowels.delete(vowels)
+    return sen 
+}
+console.log("acha to hum chlte hai")
 
